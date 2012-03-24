dc33b7294b1a201d8b57ff35678d7bc52da7f6aa
HAJIME Fukuna <f97one@gmail.com>
2012-03-24 02:23:13 +0000

    Androidアプリ「カー計簿」のプロジェクトを再度インポート
    git-svn-id: http://morriggan.formula97.ddo.jp/svn/android/trunk/Car-Kei-Bo@16 c8498cfa-7e64-e111-b453-021fc611399b

---
+++ gen/net/formula97/andorid/car_kei_bo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.formula97.andorid.car_kei_bo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/net/formula97/andorid/car_kei_bo/AddMyCar.java
@@ -0,0 +1,13 @@
+package net.formula97.andorid.car_kei_bo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddMyCar extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fuelmileageadd);
+    }
+}
