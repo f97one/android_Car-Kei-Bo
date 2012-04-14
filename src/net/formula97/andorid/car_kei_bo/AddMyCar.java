b7cd5a2f0690847d8605d3c4650cf6c5ed7ae8d3
HAJIME Fukuna <f97one@gmail.com>
2012-04-14 10:15:36 +0000

    onResume()に、ボタンの幅を調整する処理を追加。 refs #5 @0.25h
    git-svn-id: http://morriggan.formula97.ddo.jp/svn/android/trunk/Car-Kei-Bo@21 c8498cfa-7e64-e111-b453-021fc611399b

--- src/net/formula97/andorid/car_kei_bo/AddMyCar.java
+++ src/net/formula97/andorid/car_kei_bo/AddMyCar.java
@@ -2,12 +2,54 @@ package net.formula97.andorid.car_kei_bo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class AddMyCar extends Activity {
+
+	// ウィジェットを扱うための定義
+	TextView textview_addCarName = (TextView)findViewById(R.id.textview_addCarName);
+	CheckBox checkbox_setDefault = (CheckBox)findViewById(R.id.checkBox_SetDefault);
+	Button button_addCar = (Button)findViewById(R.id.button_addCar);
+	Button button_cancel_addCar = (Button)findViewById(R.id.button_cancel_addCar);
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fuelmileageadd);
+        setContentView(R.layout.addcar);
     }
+
+	/* (非 Javadoc)
+	 * @see android.app.Activity#onPause()
+	 */
+	@Override
+	protected void onPause() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onPause();
+	}
+
+	/* (非 Javadoc)
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+		/*
+		 * ボタンの配置を画面幅の1/2にする処理
+		 *
+		 * onCreate()ではなくこちらに書くのは、最終的な画面設定が行われるのがこちらという
+		 * Androidのくせによるものである。
+		 */
+
+		// 画面幅を取得
+		int displayWidth = getWindowManager().getDefaultDisplay().getWidth();
+
+		// ボタンの幅を、取得した画面幅の1/2にセット
+		button_addCar.setWidth(displayWidth / 2);
+		button_cancel_addCar.setWidth(displayWidth / 2);
+	}
 }
