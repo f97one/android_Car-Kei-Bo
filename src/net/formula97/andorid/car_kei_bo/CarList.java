b039fcb58b78d9694c0c6d42d83dcb7cdaaf34a3
HAJIME Fukuna <f97one@gmail.com>
2012-05-09 15:13:37 +0000

    ・各メソッドの最後にclose()を追加。 ・DBを参照しかしないので、getWritableDatabase()からgetReadableDatabase()に変更。 ・エレメントのレイアウトを変更したことにより、Adapterに渡す値をひとつ増加。 refs #12 @0.5h
    git-svn-id: http://morriggan.formula97.ddo.jp/svn/android/trunk/Car-Kei-Bo@55 c8498cfa-7e64-e111-b453-021fc611399b

--- src/net/formula97/andorid/car_kei_bo/CarList.java
+++ src/net/formula97/andorid/car_kei_bo/CarList.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,6 +19,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.preference.PreferenceActivity;
 
@@ -31,9 +33,12 @@ public class CarList extends Activity {
 	public static SQLiteDatabase db;
 
 	// ウィジェットを扱うための定義
-    Button button_addFuelRecord;
-    ListView listView_CarList;
+    TextView textView_CarListTitleContainer;
     TextView tv_label_value_defaultcar;
+    TableLayout TableLayout1;
+    ListView listView_CarList;
+    Button button_addFuelRecord;
+
     /**
 	 *
 	 */
@@ -51,9 +56,10 @@ public class CarList extends Activity {
 
         // ウィジェット
         //   プログラムから扱うための定数を検索してセット
+        textView_CarListTitleContainer = (TextView)findViewById(R.id.textView_CarListTitleContainer);
+        tv_label_value_defaultcar = (TextView)findViewById(R.id.tv_label_value_defaultcar);
         button_addFuelRecord = (Button)findViewById(R.id.button_addFuelRecord);
         listView_CarList = (ListView)findViewById(R.id.listView_CarList);
-        tv_label_value_defaultcar = (TextView)findViewById(R.id.tv_label_value_defaultcar);
 
 	}
 
@@ -84,8 +90,10 @@ public class CarList extends Activity {
 		 * ....て、別クラスにすれば修正箇所を集約できると思うが、できんのか？
 		 */
 
-		// 設定画面を呼び出すためのインテント宣言
-		Intent configActivity = new Intent(this, Config.class);
+		// 別画面呼び出しのためのインテント宣言
+		Intent configActivity = new Intent(this, Config.class);		// 設定画面
+		Intent addCarActivity = new Intent(this, AddMyCar.class);	// 「クルマを追加」画面
+		//Intent carListActivity = new Intent(this, CarList.class);	// 「クルマリスト」画面
 
 		switch (item.getItemId()) {
 		case R.id.optionsmenu_closeAPP:
@@ -97,8 +105,13 @@ public class CarList extends Activity {
 			startActivity(configActivity);
 			return true;
 		case R.id.optionsmenu_addcar:
+			// 「クルマを追加」画面を呼び出す
+			startActivity(addCarActivity);
 			return true;
 		case R.id.optionsmenu_carlist:
+			// 「クルマリスト」画面を呼び出す
+			//   CarListはこのクラスなので、呼び出しは行わずtrueのみ返す
+			//startActivity(carListActivity);
 			return true;
 		default:
 			return false;
@@ -112,7 +125,7 @@ public class CarList extends Activity {
 	protected void onPause() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onPause();
-		db.close();
+		dbman.close();
 	}
 
 	/* (非 Javadoc)
@@ -122,7 +135,7 @@ public class CarList extends Activity {
 	protected void onDestroy() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onDestroy();
-		db.close();
+		dbman.close();
 	}
 
 	/* (非 Javadoc)
@@ -134,28 +147,32 @@ public class CarList extends Activity {
 		super.onResume();
 
         // 編集可能な状態でDBを開く
-		db = dbman.getWritableDatabase();
+		db = dbman.getReadableDatabase();
 
 		// クルマリストとデフォルトカーの表示処理
-        //ListView listView_CarList = (ListView)findViewById(R.id.listView_CarList);
-        //TextView tv_label_value_defaultcar = (TextView)findViewById(R.id.tv_label_value_defaultcar);
-
         // クルマリストのArrayをつくる
         //   count()の結果が1レコード以上ないとAdapterが作成できないので、CAR_MASTERに１レコード以上あるかを調べ、
         //   あった場合のみAdapterをつくる
         if (dbman.hasCarRecords(db)) {
-            Cursor cCarList = dbman.getCurrentMileageData(db);
-	        cCarList.moveToFirst();
-	        String[] from = {"A.CAR_NAME", "CURRENT_FUEL_MILEAGE", "CURRENT_RUNNING_CONSTS"};
-	        int[] to = {R.id.ctv_element_CarName, R.id.tv_value_FuelMileage, R.id.tv_value_RunningCosts};
+        	// Adapterのもととなるレコードの取得
+            Cursor cCarList = dbman.getCarList(db);
+            Log.i("CAR_MASTER", "Got " + String.valueOf(cCarList.getCount()) + " records, including " + String.valueOf(cCarList.getColumnCount()) + " columns.");
+            for (int i =0; i < cCarList.getColumnCount(); i++) {
+            	Log.i("CAR_MASTER", "name of Column Index " + String.valueOf(i) + ":" + cCarList.getColumnName(i));
+            }
+
+	        // AdapterからListViewへ差し込むデータの整形
+	        String[] from = {"_id", "CAR_NAME", "CURRENT_FUEL_MILEAGE", "CURRENT_RUNNING_COST"};
+	        int[] to = {R.id.tv_carID, R.id.tv_element_CarName, R.id.tv_value_FuelMileage, R.id.tv_value_RunningCosts};
 
 	        SimpleCursorAdapter sca = new SimpleCursorAdapter(getApplicationContext(), R.layout.listviewelement_carlist, cCarList, from, to);
 	        listView_CarList.setAdapter(sca);
 
 	        // デフォルトカーの名前を取得してセット
-
-	        tv_label_value_defaultcar.setText(dbman.getDefaultCarId(db));
+	        tv_label_value_defaultcar.setText(dbman.getDefaultCarName(db));
         }
+
+        dbman.close();
 	}
 
 }
