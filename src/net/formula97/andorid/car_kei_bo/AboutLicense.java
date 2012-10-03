d44e80c9c39dd5bda076246ab27a13b8bd6505b6
HAJIME Fukuna <f97one@gmail.com>
2012-10-03 14:06:27 +0000

    ライセンス表示用Activityを追加。
    git-svn-id: http://morriggan.formula97.ddo.jp/svn/android/branches/Car-Kei-Bo/rev0_7-stable@211 c8498cfa-7e64-e111-b453-021fc611399b

---
+++ src/net/formula97/andorid/car_kei_bo/AboutLicense.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+package net.formula97.andorid.car_kei_bo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author kazutoshi
+ *
+ */
+public class AboutLicense extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.aboutlicense);
+	}
+
+}
